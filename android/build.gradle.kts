2de1e66a6cc98e62f29f9dc4df41cb0ff3b48fe3
raufimohamadfauzi-dev <raufimohamadfauzi@gmail.com>
2026-08-26 20:55:26 +0700

    build(android): suppress obsolete Java 8 javac warnings from plugin subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,14 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Plugin pihak ketiga masih mengompilasi dengan source/target 8; ini menyembunyikan
+// peringatan javac "source value 8 is obsolete" tanpa mengubah perilaku build.
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
